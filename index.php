e128c5e57b8ac398ee2997b8bfb9274f71e52314
NehalBhagat <nehal3693@hotmail.com>
2016-09-20 18:02:52 -0400

    Database info

--- index.php
+++ index.php
@@ -522,7 +522,7 @@ margin-left:0px;
 <ul>  <!-- SLIDE  -->
    <li data-index="rs-17" data-transition="fade" data-slotamount="default" data-hideafterloop="0" data-hideslideonmobile="off"  data-easein="default" data-easeout="default" data-masterspeed="default"  data-thumb="images/include/top-slider-100x50.jpg"  data-rotate="0"  data-saveperformance="off"  data-title="Slide" data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
       <!-- MAIN IMAGE -->
-      <img src=""  alt="" title="top-slider"  width="1600" height="900" data-lazyload="images/include/top-slider.jpg" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg" data-no-retina>
+      <img src=""  alt="" title="top-slider"  width="1600" height="900" data-lazyload="images/include/kt_home.jpg" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg" data-no-retina>
                                  <!-- LAYERS -->
                                                 <!-- LAYER NR. 1 -->
                                                 <div class="tp-caption    thim-slider-sub-heading" 
