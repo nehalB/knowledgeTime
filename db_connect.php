7ae1daf60532878245e5666be962f0ab39aad430
NehalBhagat <nehal3693@hotmail.com>
2016-09-20 18:01:46 -0400

    Database info

---
+++ db_connect.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nehal
+ * Date: 9/19/2016
+ * Time: 6:06 PM
+ */
+
+$db_host = "localhost";
+$db_username = "root";
+$db_pass = "";
+$db_name = "knowledgetimedb";
+
+
